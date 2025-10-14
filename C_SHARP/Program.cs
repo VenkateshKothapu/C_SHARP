981b9c6f632e6b478dcbe677a894f5c5ff03a5ae
venkatesh <venkateshreddy.fullstack@gmail.com>
2025-10-14 11:44:23 +0530

    firstprogram

---
+++ C_SHARP/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace VariablesDemo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int age = 25;
+            string name = "Venkatesh";
+            double salary = 55000.75;
+            bool isActive = true;
+
+            Console.WriteLine("Name: " + name);
+            Console.WriteLine("Age: " + age);
+            Console.WriteLine("Salary: " + salary);
+            Console.WriteLine("Active Employee: " + isActive);
+        }
+    }
+}
