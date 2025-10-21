a6b2f70bdd4a751b333ffc745d5d88a010a4bf95
venkatesh <venkateshreddy.fullstack@gmail.com>
2025-10-21 10:12:59 +0530

    variables and conversations

---
+++ C_SHARP/Conversion.cs
@@ -0,0 +1,98 @@
+﻿using System;
+
+
+namespace csharp
+{
+    class Conversion
+    {
+        static void Main(string[] args)
+        {
+
+            //implicit conversion: to convert small datatype into large datatype
+
+            byte age = 15;
+            int updatedage = age;
+            Console.WriteLine($"value: {updatedage}");
+            Console.WriteLine($"Type: {updatedage.GetType()}");
+
+            int number = 200000;
+            long updatednumber = number;
+            Console.WriteLine($"value is: {updatednumber}");
+            Console.WriteLine($"Type is: {updatednumber.GetType()}");
+
+
+            int rank = 10000;
+            double updatedRank = rank;
+            Console.WriteLine($"value is: {updatedRank}");
+            Console.WriteLine($"Type is: {updatedRank.GetType()}");
+
+
+
+            //explicit conversions
+            int num = 20000;
+            ulong updatednum = (ulong)num;
+            Console.WriteLine($"value is: {updatednum}");
+            Console.WriteLine($"Type is: {updatednum.GetType()}");
+
+
+            int marks = 1000;
+            byte updatedMarks = (byte)marks;
+            Console.WriteLine($"value: {updatedMarks}");
+            Console.WriteLine($"type: {updatedMarks.GetType()}");
+
+
+            double percentage = 9.999;
+            int updatedPercentage = (int)percentage;
+            Console.WriteLine($"value: {updatedPercentage}");
+            Console.WriteLine($"type: {updatedPercentage.GetType()}");
+
+
+            int value = 90102441;
+            string updatedvalue = value.ToString();
+            Console.WriteLine($"value: {updatedvalue}");
+            Console.WriteLine($"type: {updatedvalue.GetType()}");
+
+
+
+            string value1 = "90102441";
+            int updatedvalue1 = int.Parse(value1);
+            Console.WriteLine($"value: {updatedvalue1}");
+            Console.WriteLine($"type: {updatedvalue1.GetType()}");
+
+
+
+            string value2 = "90102441";
+            int updatedvalue2 = Convert.ToInt32(value2);
+            Console.WriteLine($"value: {updatedvalue2}");
+            Console.WriteLine($"type: {updatedvalue2.GetType()}");
+
+
+            //value type numeric types
+            string nullbaleString = null;
+            int output = Convert.ToInt32(nullbaleString);
+            Console.WriteLine($"value: {output}");//0
+            Console.WriteLine($"type: {output.GetType()}");
+
+
+            DateTime datetime = DateTime.Now;
+            Console.WriteLine(datetime.ToString());
+
+
+            //get age of the person
+            DateTime birthdate = new DateTime(2003, 12, 11);
+            DateTime today = DateTime.Now;
+            int age1 = today.Year - birthdate.Year;
+            Console.WriteLine(age1);
+
+
+            //Age calculator
+            Console.Write("Enter your birth year: ");
+            int birthYear = int.Parse(Console.ReadLine());
+
+            int currentYear = DateTime.Now.Year;
+            int currentAge = currentYear - birthYear;
+
+            Console.WriteLine($"You are {currentAge} years old.");
+        }
+    }
+}
---
+++ C_SHARP/Variables.cs
@@ -0,0 +1,115 @@
+﻿using System;
+using System.Diagnostics.CodeAnalysis;
+using System.Runtime.InteropServices;
+using static System.Net.Mime.MediaTypeNames;
+
+
+namespace variables
+{
+     class Variables
+    {
+        static void Main(string[] args)
+        {
+            //int age = 22;
+            //float height = 5.9F;
+            //double weight = 72.45;
+            //char grade = 'A';
+            //bool isPassed = true;
+            //string name = "Venkatesh";
+
+            //Console.WriteLine($"Name: {name}, Age: {age}, Height: {height}, Weight: {weight}, Grade: {grade}, Passed: {isPassed}");
+
+
+
+
+            //Imagine you’re building a small HR System for your company.
+
+            //string employeeName = "Ravi Kumar";
+            //int employeeId = 1021;
+            //double salary = 45000.50;
+            //bool isPermanent = true;
+            //Console.WriteLine($"Employee {employeeName} (ID: {employeeId}) earns {salary} and permanent status is {isPermanent}");
+
+            //  //    Variables are memory containers that store values.
+
+            //     Data type decides what type of data can be stored.
+
+            //      Naming should be meaningful and follow rules.
+
+            //       //C# is strongly typed, meaning every variable must have a defined type.
+            //       
+
+
+
+            //string studentname = "VENKY";
+            //int rollnumber = 62;
+            //int clas=10;
+
+            //Console.WriteLine($"student name :{studentname}");
+            //Console.WriteLine($"student rollno is :{rollnumber}");
+            //Console.WriteLine($"Student is in  class :{ clas}");
+
+
+
+
+
+
+            //now im trying to write a program which includes all of these
+
+
+            int age = 25;
+            float height = 5.10f;
+            double weight = 72.45;
+            char grad= 'A';
+            bool passed = true;
+            string name = "ram";
+
+            Console.WriteLine($"age of the student is :{age}");
+            Console.WriteLine($"Height of the student is :{height}");
+            Console.WriteLine($"weight of the student is :{weight}");
+            Console.WriteLine($"grade for student is :{grad}");
+            Console.WriteLine($"student has passed in sem : {passed}");
+            Console.WriteLine($"Name of the student is :{name}");
+
+
+            //now we will see about operators 
+
+            int a = 10, b = 25;
+            int sum = a + b;
+            //here + is an operator and it adds a and b
+
+            Console.WriteLine("Addition: " + (a + b));
+            Console.WriteLine("Subtraction: " + (a - b));
+            Console.WriteLine("Multiplication: " + (a * b));
+            Console.WriteLine("Division: " + (a / b));
+            Console.WriteLine("Remainder: " + (a % b));
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+        }
+
+     }
+}
